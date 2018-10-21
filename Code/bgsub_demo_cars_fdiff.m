66f497a4b58c80a0b50a069c8886437f852481d6
Giannis Tsagatakis <jtsagata@gmail.com>
2018-10-21 19:46:12 +0200

    1st method somehow working

--- Code/bgsub_demo_cars_fdiff.m
+++ Code/bgsub_demo_cars_fdiff.m
@@ -1,7 +1,9 @@
 clearvars;
 close all
 
-load('cars.mat');
+load('../Data/cars.mat');
 video_noBg = bgsub_frame_diff(ImSeq, 3, 60);
 video_annot = anotate_video_box(video_noBg);
+
 videoSave('../Videos/bgsub_framediff_cars.avi',video_annot,10);
+video_to_img_seq(video_annot,'../Videos/bgsub_framediff_cars.png');
